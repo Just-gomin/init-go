a8d5e27943cc6c7de912b28b0be4ff0252b69bfa
안강민 <hkkm612@gmail.com>
2022-06-11 11:39:13 +0900

    Using Variable

--- hello/hello.go
+++ hello/hello.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println("Hello World")
+	// var keyword를 이용해 변수 선언을 시작
+	// var - 변수 명 - 변수 타입 - [= 값] 을 통해 메모리 할당
+	var a int = 10
+	var msg string = "Hello Variable"
+
+	// 변수의 값 변경
+	a = 20
+	msg = "Good Morning"
+
+	// 변수를 이용해 값을 전달
+	fmt.Println(msg, a)
 }
