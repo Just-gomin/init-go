340768ddaeb24e4b919b79b41bfddb07634f29d9
안강민 <hkkm612@gmail.com>
2022-06-11 15:09:19 +0900

    Type Casting

--- hello/hello.go
+++ hello/hello.go
@@ -1,14 +1,21 @@
 package main
 
+import "fmt"
+
 func main() {
-	// 잘못된 타입 변수 사용
-	// Go는 강타입 언어 중에서도 타입 검사를 가장 엄격하게 합니다.
+	// 타입 변환
+	// 다른 타입으로 변환하고자 하는 경우,
+	// 타입명 뒤에 괄호를 쓰고 그안에 변수를 넣음으로써 타입 변환이 가능합니다.
 	a := 3
 	var b float64 = 3.5
 
-	var c int = b // 타입이 다른 변수 대입 불가
-	d := a * b    // 타입이 다른 변수들 끼리 연산 불가
+	var c int = int(b)  // float64 3.5 -> int 3 :: 실수형에서 정수형으로의 변화시에는 소수점아래의 값을 "버림" 합니다.
+	d := float64(a * c) // int -> float64
 
 	var e int64 = 7
-	f := a * e // 정수 값이더라도 엄연히 다른 타입이기 때문에 연산 불가
+	f := int64(d) * e // float64 -> int64
+
+	var g int = int(b * 3) // float64 -> int로 형 변환 전에 연산을 진행해, 3.5 * 3 = 10.5가 된 후 형 변환을 진행
+	var h int = int(b) * 3 // float64 -> int로 형 변환을 선행 후 연산을 진행 해 3 * 3 = 9로 연산
+	fmt.Println(g, h, f)
 }
