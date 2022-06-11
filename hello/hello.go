f02cf54020f20067ae1689a5a029cdb9737ae6e1
안강민 <hkkm612@gmail.com>
2022-06-11 14:40:16 +0900

    Strong Type Check

--- hello/hello.go
+++ hello/hello.go
@@ -1,20 +1,14 @@
 package main
 
-import "fmt"
-
 func main() {
-	var a int = 3
-	var b int
-	var c = 4 // type 생략, 우변의 타입으로 지정
-	d := 5    // 선언 대입문 : var 와 type 생략
-	fmt.Println(a, b, c, d)
+	// 잘못된 타입 변수 사용
+	// Go는 강타입 언어 중에서도 타입 검사를 가장 엄격하게 합니다.
+	a := 3
+	var b float64 = 3.5
 
-	var float32DefaultVal float32
-	fmt.Println(float32DefaultVal) // 0.0
+	var c int = b // 타입이 다른 변수 대입 불가
+	d := a * b    // 타입이 다른 변수들 끼리 연산 불가
 
-	var booleanDefaultVal bool
-	fmt.Println(booleanDefaultVal) // false
-
-	var stringDefaultVal string
-	fmt.Println(stringDefaultVal) // ""
+	var e int64 = 7
+	f := a * e // 정수 값이더라도 엄연히 다른 타입이기 때문에 연산 불가
 }
