c9881c4168fdbb1568bb068539e626be8a097cc6
안강민 <hkkm612@gmail.com>
2022-07-31 23:29:15 +0900

    Custom Logger 추가

---
+++ logger/go.mod
@@ -0,0 +1,3 @@
+module init-go/logger
+
+go 1.18
---
+++ logger/logger.go
@@ -0,0 +1,36 @@
+package logger
+
+import "fmt"
+
+/* -------------------------- */
+/*           Colors           */
+/* -------------------------- */
+const red string = "\033[31m"
+const green string = "\033[32m"
+const yellow string = "\033[33m"
+const blue string = "\033[34m"
+const purple string = "\033[35m"
+const cyan string = "\033[36m"
+const white string = "\033[37m"
+
+/* -------------------------- */
+/*       Format String        */
+/* -------------------------- */
+const warnString = yellow + "[" + "%s" + "]" + white + " " + // specialchanr
+	cyan + "[" + "%-20s" + "]" + white + " " + // title
+	": %s\n" // body
+
+const errorString = red + "[" + "%s" + "]" + white + " " + // specialchanr
+	cyan + "[" + "%-20s" + "]" + white + " " + // title
+	": %s\n" // body
+
+/* -------------------------- */
+/*       Print Function       */
+/* -------------------------- */
+func Warning(specialChar string, title string, body string) {
+	fmt.Printf(warnString, specialChar, title, body)
+}
+
+func Error(specialChar string, title string, content string) {
+	fmt.Printf(errorString, specialChar, title, content)
+}
