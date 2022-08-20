5e8bb3ff5f408508ba3622e79419edb8f7a94b6e
안강민 <hkkm612@gmail.com>
2022-08-20 16:47:12 +0900

    가변인수 함수 추가
    - 가변인수로 전달된 매개변수의 타입은 슬라이스
    - 매개변수의 타입을 슬라이스로 바꾼다 해서 가변인수 함수로 사용할 수 없음 확인

--- functions/variadic/variadic.go
+++ functions/variadic/variadic.go
@@ -4,49 +4,75 @@ import "fmt"
 
 func main() {
 	generalFunc("hi", 6, true)
+
+	fmt.Println(sum(1, 2, 3, 4, 5))
+	// fmt.Println(sum2(1, 2, 3, 4, 5, 6))
+	fmt.Println(sum())
+	fmt.Println(sum2([]int{1, 2, 3}))
 }
+
 func generalFunc(args ...interface{}) {
+	fmt.Printf("args Type : %T\n", args)
+
 	for _, arg := range args {
 		switch f := arg.(type) {
 
 		case bool:
 			val := arg.(bool)
-			fmt.Printf("[%t] %t", f, val)
+			fmt.Printf("[%T] %t\n", f, val)
 
 		case string:
 			val := arg.(string)
-			fmt.Printf("[%s] %s", f, val)
+			fmt.Printf("[%T] %s\n", f, val)
 
 		case int:
 			val := arg.(int)
-			fmt.Printf("[%c] %c", f, val)
+			fmt.Printf("[%T] %c\n", f, val)
 
 		case int8:
 			val := arg.(int8)
-			fmt.Printf("[%c] %c", f, val)
+			fmt.Printf("[%T] %c\n", f, val)
 
 		case int16:
 			val := arg.(int16)
-			fmt.Printf("[%c] %c", f, val)
+			fmt.Printf("[%T] %c\n", f, val)
 
 		case int32:
 			val := arg.(int32)
-			fmt.Printf("[%c] %c", f, val)
+			fmt.Printf("[%T] %c\n", f, val)
 
 		case int64:
 			val := arg.(int64)
-			fmt.Printf("[%c] %c", f, val)
+			fmt.Printf("[%T] %c\n", f, val)
 
 		case float32:
 			val := arg.(float32)
-			fmt.Printf("[%f] %f", f, val)
+			fmt.Printf("[%T] %f\n", f, val)
 
 		case float64:
 			val := arg.(float64)
-			fmt.Printf("[%f] %f", f, val)
+			fmt.Printf("[%T] %f\n", f, val)
 			/*
 				다른 타입들에 대해서 위와 같은 로직
 			*/
 		}
 	}
+}
+
+func sum(nums ...int) int {
+	sum := 0
+	fmt.Printf("nums 타입: %T\n", nums)
+	for _, v := range nums {
+		sum += v
+	}
+	return sum
+}
+
+func sum2(nums []int) int {
+	sum := 0
+	fmt.Printf("nums 타입: %T\n", nums)
+	for _, v := range nums {
+		sum += v
+	}
+	return sum
 }
